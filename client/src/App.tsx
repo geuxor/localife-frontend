bbfc8ad7f3c7cba55d773abdc69f307e92631b04
Seb Green <sebastian_green@live.com>
2021-08-16 15:02:16 +0100

    merge

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,18 @@
 import LandingPage from './pages/landingPage/landingPage'
+import SearchForm from './components/searchForm/searchForm'
+import { BrowserRouter } from 'react-router-dom'
 import './App.css'
 
 function App() {
-  return <LandingPage />
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <LandingPage />
+        <SearchForm />
+      </BrowserRouter>
+      <header className="App-header"></header>
+    </div>
+  )
 }
 
 export default App

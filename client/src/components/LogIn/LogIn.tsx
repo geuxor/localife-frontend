d747770887f43c905038b98100a4abe253e0c4a3
Maria Pallares <mpallareslara@gmail.com>
2021-08-17 16:38:53 +0200

    HTML in the log in form created

--- client/src/components/LogIn/LogIn.tsx
+++ client/src/components/LogIn/LogIn.tsx
@@ -2,8 +2,22 @@ import React from 'react'
 
 export default function LogIn() {
   return (
-    <div>
-      
-    </div>
+    <form>
+      <div className="form-inner">
+        <h2>LOGIN</h2>
+        <div className="form-group">
+          <label htmlFor="name">Name:</label>
+          <input type="text" name="name" id="name" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">EMAIL</label>
+          <input type="email" name="email" id="email" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">PASSWORD</label>
+          <input type="password" name="password" id="password" />
+        </div>
+      </div>
+    </form>
   )
 }

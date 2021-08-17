78ebdbb693f7383b40d852684c8e8e94d3e70ec3
Seb Green <sebastian_green@live.com>
2021-08-17 12:26:00 +0100

    added button logic

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,16 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
 import NavBar from './components/NavBar/NavBar'
+import { Provider } from 'react-redux'
+import store from './redux/store'
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <NavBar />
+      </div>
+    </Provider>
   )
 }
 

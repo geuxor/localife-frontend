1d2b484a40a74caab434fa2afd13145358239116
Maria Pallares <mpallareslara@gmail.com>
2021-08-18 13:28:49 +0200

    Pop up for login, and close button, cleaned navbar

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -6,33 +6,11 @@ import LogIn from '../LogIn/LogIn'
 import { SET_USER, SET_LOGIN } from '../../redux/actions/actions'
 
 function NavBar() {
-  const dispatch = useDispatch()
-
-  const [showLogIn, setShowLogIn] = useState(false)
-
-  function logInUser(details) {
-    if (
-      details.email == adminUser.email &&
-      details.password == adminUser.password
-    ) {
-      dispatch({ type: SET_USER, payload: {} })
-    } else {
-      console.log('details do not match')
-    }
-  }
-
-  function logOut() {
-    dispatch({ type: SET_USER, payload: {} })
-  }
-
   const [currentUser, setCurrentUser] = useState(false)
   const [showLogin, setShowLogin] = useState(false)
   const [showRegister, setShowRegister] = useState(false)
 
-  const adminUser = {
-    email: 'admin@admin.com',
-    password: 'admin123',
-  }
+  const dispatch = useDispatch()
 
   const state = useSelector((state: RootState) => state)
 

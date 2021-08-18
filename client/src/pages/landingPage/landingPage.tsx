6fccc4d7f6298f15bbe3a2aed56dbb838b03eaae
Seb Green <sebastian_green@live.com>
2021-08-18 08:35:17 +0100

    Update landingPage.tsx

--- client/src/pages/landingPage/landingPage.tsx
+++ client/src/pages/landingPage/landingPage.tsx
@@ -1,5 +1,5 @@
 import SearchForm from '../../components/searchForm/searchForm'
-import Navbar from '../../components/navBar/NavBar'
+import Navbar from '../../components/NavBar/NavBar'
 import './landingPage.css'
 
 export default function LandingPage() {

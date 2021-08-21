8bc48013838befd30abd2f69aaba8c3e4e815e62
German B <geuxor@dr.com>
2021-08-21 13:44:03 +0200

    -mremoved unused warnings

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,6 @@
 import './App.css'
 import { BrowserRouter as Router } from 'react-router-dom'
 import Routes from './routes/routes'
-import NavBar from './components/NavBar/NavBar'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import Menu from './components/Menu/Navbar'
--- client/src/apiServices/experiencesApi.tsx
+++ client/src/apiServices/experiencesApi.tsx
@@ -7,21 +7,11 @@ const options: any = {
 }
 
 ExperiencesApi.searchExperiencesApi = async (searchLocation) => {
-  const options: any = {
-    headers: { 'Content-Type': 'application/json' },
-    method: 'post',
-  }
-  try {
-    const res = await axios.post(
-      'http://localhost:4001/search-results',
-      searchLocation,
-      options,
-    )
-    // console.log('API response ===>', res.data)
-    return res.data
-  } catch (err) {
-    console.log(err)
-  }
+  return await axios.post(
+    `${process.env.REACT_APP_API}/search-results`,
+    searchLocation,
+    options,
+  )
 }
 
 ExperiencesApi.viewExperience = async (experienceId) => {

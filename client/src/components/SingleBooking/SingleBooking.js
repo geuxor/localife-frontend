b5f1766e3653e1950f240dd3026acf8783673c4f
Maria Pallares <mpallareslara@gmail.com>
2021-08-24 11:40:35 +0200

    Logic my bookings implemented, styling current upcoming list of experiences

--- client/src/components/SingleBooking/SingleBooking.js
+++ client/src/components/SingleBooking/SingleBooking.js
@@ -1,29 +1,44 @@
-import React, {useSelector} from 'react'
+import React, { useSelector } from 'react'
 import './SingleBooking.css'
+import moment from 'moment'
 
-export default function SingleBooking({booking}) {
+export default function SingleBooking({ booking }) {
+  const formatDay = 'DD/MM/YYYY'
+  const formatTime = 'HH:mm'
 
-
-  console.log(booking.Experience)
   return (
-    <div className="exp-container2">
+    <div className="booking-container2">
       <img
         src={booking.Experience.image}
         alt="experience"
         className="experience-image"
       />
-      <div className="exp-container3">
-        <div className="exp-container4">
-          <div className="experience-title">{booking.Experience.title}</div>
-          <div className="experience-description">{booking.Experience.description}</div>
+      <div className="booking-container3">
+        <div className="booking-container4">
+          <div className="booking-title">{booking.Experience.title}</div>
+          <div className="booking-description">
+            {booking.Experience.description}
+          </div>
         </div>
-        <div className="exp-container5">
-          <b>
-            <div className="exp-price">
-              <i className="fas fa-euro-sign"></i>
-              {booking.price}.00
-            </div>
-          </b>
+        <div className="booking-details-container">
+          <div className="booking-price">
+            <i className="fas fa-euro-sign"></i>
+            {booking.price}.00
+          </div>
+          <div className="start-date">
+            <p>{moment(booking.start_date).format(formatDay)}</p>
+            <p className="booking-time">
+              {moment(booking.start_date).format(formatTime)}
+              <i class="far fa-clock"></i>
+            </p>
+          </div>
+          <div className="end-date">
+            <p>{moment(booking.end).format(formatDay)}</p>
+            <p className="booking-time">
+              {moment(booking.end).format(formatTime)}
+              <i class="far fa-clock"></i>
+            </p>
+          </div>
         </div>
       </div>
     </div>

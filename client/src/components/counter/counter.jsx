c2f7a01411c1389c338f17ec04d13f4ecb61b063
will <william.jl.johnson@gmail.com>
2021-09-14 19:00:56 +0200

    feat: rendering no. of guests to guest box

--- client/src/components/counter/counter.jsx
+++ client/src/components/counter/counter.jsx
@@ -1,21 +1,13 @@
 import './counter.css'
-import { useState } from 'react'
 
-function Counter() {
-  const [count, setCount] = useState(0)
-
+function Counter({ onClick, onClick2, totalCount }) {
   return (
     <div className="counter-container">
-      <button
-        onClick={() => {
-          if (count !== 0) setCount(count - 1)
-        }}
-        className="counter-button"
-      >
+      <button onClick={onClick2} className="counter-button">
         -
       </button>
-      <p>{count}</p>
-      <button onClick={() => setCount(count + 1)} className="counter-button">
+      <p>{totalCount}</p>
+      <button onClick={onClick} className="counter-button">
         +
       </button>
     </div>

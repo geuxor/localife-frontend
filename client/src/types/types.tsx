94be88db2e36c67b63ef12ad4e700cdf8b6eb861
will <william.jl.johnson@gmail.com>
2021-09-16 12:43:05 +0200

    REFACTOR: TS for exp details and exp results pages

--- client/src/types/types.tsx
+++ client/src/types/types.tsx
@@ -23,20 +23,28 @@ export interface Props {
   experience: any
 }
 
-//TODO: update exp ineterface
-
-// User: null
-// UserId: null
-// createdAt: "2021-08-17T10:00:30.582Z"
-// description: "New ABC 13 9370, 13.3, 5th Gen CoreA5-8250U, 8GB RAM, 256GB SSD, power UHD Graphics, OS 10 Home, OS Office A & J 2016"
-// from: null
-// id: 1
-// image: "http://placeimg.com/640/480"
-// lat: null
-// location: "Littleton"
-// lon: null
-// price: 619
-// quantity: null
-// title: "Gorgeous iure inventore pariatur"
-// to: null
-// updatedAt: "2021-08-17T10:00:30.582Z"
+export interface ExpState {
+  experience: {
+    User: {
+      avatar: string
+      first_name: string
+    }
+    UserId: number
+    city: string
+    country: string
+    createdAt: string
+    description: string
+    from: string
+    id: number
+    image: string
+    lat: number
+    location: string
+    lon: number
+    price: number
+    quantity: number
+    subtitle: string
+    title: string
+    to: string
+    updatedAt: string
+  }[]
+}
